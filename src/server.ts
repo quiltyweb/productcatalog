3caed445f98f25b8b1e3296b52b695ddf30b847f
Craig Franklin <craigjfranklin@gmail.com>
2020-04-12 09:41:10 +1000

    Add apollo server for GQL API

--- src/server.ts
+++ src/server.ts
@@ -2,17 +2,36 @@ import 'reflect-metadata'
 import Koa from 'koa'
 import Router from 'koa-router'
 import { createConnection } from 'typeorm'
+import { ApolloServer, gql } from 'apollo-server-koa'
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 createConnection().then(async connection => {
   const app = new Koa()
   const router = new Router()
 
-  router.get('/*', async (ctx) => {
+  const typeDefs = gql`
+    type Query {
+      hello: String
+    }
+  `
+  const resolvers = {
+    Query: {
+      hello: (): string => 'world'
+    }
+  }
+
+  const server = new ApolloServer({ typeDefs, resolvers })
+  app.use(server.getMiddleware())
+
+  router.get('/', async (ctx) => {
     ctx.body = 'Hello World!'
   })
 
-  app.use(router.routes())
+  app
+    .use(router.routes())
+    .use(router.allowedMethods())
+
+  server.applyMiddleware({ app })
 
   app.listen(3000)
 
