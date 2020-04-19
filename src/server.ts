13d329566ff424fa00e3ddefd83826547b546cee
Craig Franklin <craigjfranklin@gmail.com>
2020-04-19 14:23:04 +1000

    Add basic session functionality

--- src/server.ts
+++ src/server.ts
@@ -5,21 +5,44 @@ import { createConnection } from "typeorm";
 import { ApolloServer } from "apollo-server-koa";
 import helmet from "koa-helmet";
 import cors from "@koa/cors";
+import session from "koa-session";
 
 import loadSchema from "./graphql";
 import Email from "./email";
 
+import type { Context as KoaContext } from "koa";
+
 const connectionName =
   process.env.NODE_ENV === "development" ? "default" : process.env.NODE_ENV;
 
-createConnection(connectionName)
+const CONFIG = {
+  key: "koa:sess",
+  maxAge: 86400000,
+  autoCommit: true,
+  overwrite: true,
+  httpOnly: true,
+  signed: true,
+  rolling: false,
+  renew: false,
+  sameSite: null,
+};
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+createConnection()
   .then(async (connection) => {
     const app = new Koa();
     const router = new Router();
 
+    if (!process.env.APP_KEY)
+      throw Error("An APP_KEY is required to sign session cookies.");
+
+    app.keys = [process.env.APP_KEY];
+
     const schema = await loadSchema(connection);
-    const context = { sendEmail: Email.send };
-    const server = new ApolloServer({ schema, context });
+    const server = new ApolloServer({
+      schema,
+      context: ({ ctx }): KoaContext => ({ ...ctx, sendEmail: Email.send }),
+    });
 
     router.get("/", async (ctx) => {
       ctx.body = "Hello World!";
@@ -28,6 +51,7 @@ createConnection(connectionName)
     app
       .use(cors())
       .use(helmet())
+      .use(session(CONFIG, app))
       .use(router.routes())
       .use(router.allowedMethods());
 
@@ -37,4 +61,4 @@ createConnection(connectionName)
 
     console.log(`Server running on port ${process.env.PORT}`);
   })
-  .catch((error) => console.log(error));
+  .catch((error) => console.error(error));
