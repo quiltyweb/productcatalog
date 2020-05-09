5b2e730b064998b31bb6fd9e683af6feb8c32ae4
melissa gattoni <meligatt@gmail.com>
2020-05-09 17:08:24 +1000

    Update create react app to use Typescript

--- client/src/pages/home/Home.tsx
+++ client/src/pages/home/Home.tsx
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.png';
+import './Home.css';
 
-function App() {
+const Home: React.FunctionComponent = () => {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          product catalog!
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.gattoni.cl"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          gattoni.cl v1
         </a>
       </header>
+      <main>
+        Home page
+      </main>
     </div>
   );
 }
 
-export default App;
+export default Home;
