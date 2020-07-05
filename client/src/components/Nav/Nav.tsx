3b1ad3c04891007413dcb1b2594826944aae946e
melissa gattoni <meligatt@gmail.com>
2020-07-05 21:47:01 +1000

    add mini search to nav

--- client/src/components/Nav/Nav.tsx
+++ client/src/components/Nav/Nav.tsx
@@ -1,10 +1,10 @@
 import React from "react";
+import { useFormik } from "formik";
 import { styled } from "fannypack";
 import { Link } from "react-router-dom";
+import { InputField, Button } from "fannypack";
 // import hillsSVg from './hills.svg';
 
-// TODO: avatar image can be as a background-image in the styled components,
-// with a background-size: cover to preserve the aspect ratio.
 const NavContainer = styled.nav`
   display: flex;
   align-items: center;
@@ -16,12 +16,12 @@ const NavContainer = styled.nav`
   left: 0;
   margin: 0;
   padding: 0;
-  color: #D32F2F;
-  background-color: #FFFFFF;
+  color: #d32f2f;
+  background-color: #ffffff;
 
   @media (min-width: 760px) {
     flex-direction: row;
-   }
+  }
 `;
 
 const NavBarContent = styled.div`
@@ -35,11 +35,9 @@ const NavBarContent = styled.div`
   margin: 0;
   padding: 0 2rem;
 
-
   @media (min-width: 760px) {
     flex-direction: row;
-   }
-
+  }
 `;
 
 const NavLogo = styled.a`
@@ -56,10 +54,14 @@ const NavLogo = styled.a`
 const NavLogoHeading = styled.span`
   font-size: 3rem;
   font-weight: bold;
-  color: #D32F2F;
+  color: #d32f2f;
 `;
 
-const NavLogoSubHeading= styled.span`
+const FormStyled = styled.form`
+  display: flex;
+  align-items: flex-end;
+`;
+const NavLogoSubHeading = styled.span`
   font-size: 1rem;
 `;
 const NavBarList = styled.ul`
@@ -70,21 +72,54 @@ const NavBarList = styled.ul`
   margin: 0;
   padding: 0;
 
-
   @media (min-width: 760px) {
     flex-direction: row;
-   }
-
+  }
 `;
 
 const Nav = () => {
+  const formik = useFormik({
+    initialValues: {
+      searchTerm: "",
+    },
+    validate: ({ searchTerm }) => {
+      const errors: any = {};
+      if (!searchTerm) {
+        errors["searchTerm"] = "Busqueda es requerido";
+      }
+      return errors;
+    },
+    onSubmit: (values) => {
+      console.log("onSubmit", values);
+    },
+  });
   return (
     <NavContainer>
-       <NavBarContent>
+      <NavBarContent>
         <NavLogo href="/" title="Gattoni">
           <NavLogoHeading>GATTONI</NavLogoHeading>
           <NavLogoSubHeading>Seguridad Industrial</NavLogoSubHeading>
         </NavLogo>
+
+        <FormStyled onSubmit={formik.handleSubmit}>
+          <InputField
+            padding="major-1"
+            id="searchTerm"
+            name="searchTerm"
+            type="text"
+            label="Buscar"
+            placeholder="ingrese busqueda"
+            value={formik.values.searchTerm}
+            onChange={formik.handleChange}
+            validationText={formik.errors.searchTerm}
+            state={formik.errors.searchTerm ? "danger" : ""}
+            size="medium"
+            isRequired
+          />
+          <Button margin="major-1" padding="major-1" type="submit">
+            buscar
+          </Button>
+        </FormStyled>
         <NavBarList aria-label="Menu principal">
           <li>
             <Link to="/cotizacion" className="NavBar-primary-link">
