bd49b1c94c3c052d3d8fad830055d4601b8117b7
Daniela Gattoni <danigattoni@gmail.com>
2020-07-10 21:45:26 +1000

    fix: add item on click cotizar button

--- client/src/pages/CartPage/CartPage.tsx
+++ client/src/pages/CartPage/CartPage.tsx
@@ -1,13 +1,30 @@
-import React from "react";
-import { Heading } from "fannypack";
+import React from 'react';
+import {
+  Heading,
+  Paragraph,
+  Box
+} from 'fannypack'
 import CartList from "../../components/CartList/CartList";
+import QuoteForm from "../../components/QuoteForm/QuoteForm";
+import { useHomePageContext } from '../../pages/HomePage/HomePageContext';
 
 export const CartPage = () => {
- 
+  const { cart } = useHomePageContext();
+
+  if (!cart.length) {
+    return (
+      <Box>
+        <Heading>Mi Cotización:</Heading>
+        <Paragraph>No hay Productos en la Cotización.</Paragraph>
+        <Paragraph>Agregue articulos a su cotización navegando a través del menú de categorias</Paragraph>
+      </Box>
+    );
+  }
+
   return (
     <>
-      <Heading>Mi Cotizacion</Heading>
       <CartList />
+      <QuoteForm />
     </>
   );
 };
