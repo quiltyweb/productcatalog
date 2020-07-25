4dc70b591bd39f1ea57311d483c31b8b7567ec20
melissa gattoni <meligatt@gmail.com>
2020-07-25 10:52:01 +1000

    add certifictiones list

---
+++ client/src/components/CertificationsList/CertificationsList.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Heading, Paragraph } from "fannypack";
+
+const CertificationsList = () => {
+  return (
+    <>
+      <Heading>Certificaciones</Heading>
+      <Heading use="h2">
+        Con más de 20 años de experiencia en la Región de Atacama.
+      </Heading>
+      <Paragraph>
+        Comercial Gattoni le da la más cordial bienvenida y le invita a conocer
+        su amplia gama de productos de seguridad Industrial. Somos una Empresa
+        con más de 20 años de experiencia en el área de venta de artículos de
+        Seguridad Industrial, ubicada en Copiapó, Región de Atacama.
+      </Paragraph>
+    </>
+  );
+};
+
+export default CertificationsList;
