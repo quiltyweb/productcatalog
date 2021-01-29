c88508a010b12fc7a8bdcf296c2bfb253574d3d9
melissa gattoni <meligatt@gmail.com>
2021-01-29 18:19:32 +1100

    update test

--- client/src/components/ContactForm/ContactForm.test.tsx
+++ client/src/components/ContactForm/ContactForm.test.tsx
@@ -1,7 +1,6 @@
 import React from "react";
-import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import ContactForm from "./ContactForm";
-import userEvent from "@testing-library/user-event";
 
 describe("ContactForm Component", () => {
   it("renders without crashing", async () => {
@@ -16,37 +15,4 @@ describe("ContactForm Component", () => {
     screen.getByLabelText("Telefono");
     screen.getByLabelText("Consulta");
   });
-
-  // it("should submit the form", async () => {
-  //   render(<ContactForm />);
-  //   // const name = screen.getByLabelText("Nombre");
-  //   // const company = screen.getByLabelText("Empresa");
-  //   // const email = screen.getByLabelText("E-mail");
-  //   // const phone = screen.getByLabelText("Telefono");
-  //   // const message = screen.getByLabelText("Consulta");
-
-  //   // userEvent.type(name, "mock_name");
-  //   // expect(name).toHaveValue("mock_name");
-
-  //   // fireEvent.change(company, {
-  //   //   target: {
-  //   //     value: "mock_company",
-  //   //   },
-  //   // });
-  //   // fireEvent.change(email, {
-  //   //   target: {
-  //   //     value: "mock_email",
-  //   //   },
-  //   // });
-  //   // fireEvent.change(phone, {
-  //   //   target: {
-  //   //     value: "mock_phone",
-  //   //   },
-  //   // });
-  //   // fireEvent.change(message, {
-  //   //   target: {
-  //   //     value: "mock_message",
-  //   //   },
-  //   // });
-  // });
 });
