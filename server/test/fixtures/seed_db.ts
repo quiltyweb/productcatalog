cb195ef09aca2982b4ac0a14d8c645918a2dc6f0
melissa gattoni <meligatt@gmail.com>
2021-02-14 11:11:14 +1100

    update format for seed db script

--- server/test/fixtures/seed_db.ts
+++ server/test/fixtures/seed_db.ts
@@ -3,19 +3,19 @@ import { ProductFactory, CategoryFactory } from "./factories";
 import { Category } from "../../src/entity/Category";
 import { Product } from "../../src/entity/Product";
 
- (async () => {
+(async (): Promise<void> => {
   const connection: Connection = await createConnection("test");
-    const recordCount = 5;
+  const recordCount = 5;
 
-    const categories = CategoryFactory.buildMany(recordCount);
-    await connection.manager.save(Category, categories);
-    const categoryRecords = await connection.manager.find(Category);
+  const categories = CategoryFactory.buildMany(recordCount);
+  await connection.manager.save(Category, categories);
+  const categoryRecords = await connection.manager.find(Category);
 
-    const products = categoryRecords.flatMap((category: Category) =>
-      ProductFactory.buildMany(recordCount, { category })
-    );
+  const products = categoryRecords.flatMap((category: Category) =>
+    ProductFactory.buildMany(recordCount, { category })
+  );
 
-    await connection.manager.save(Product, products);
+  await connection.manager.save(Product, products);
 
-    connection.close()
+  await connection.close();
 })();
