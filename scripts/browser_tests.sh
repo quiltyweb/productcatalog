dc34e77ec409f2178c57614a5211d095f0f7e211
Craig Franklin <craigjfranklin@gmail.com>
2021-02-28 11:27:03 +1100

    Make sure environment/containers are reset in between tests
    We were getting browser test failures in CI that we couldn't
    reproduce on local, because the integration test script that
    I wrote awhile ago was hot garbage that technically worked but
    dirtied the CI environment such that server errors carried over
    into the browser tests. The changes to the script files fix this,
    but we also call 'docker-compose down' between them to be on the
    safe side.

--- scripts/browser_tests.sh
+++ scripts/browser_tests.sh
@@ -2,13 +2,11 @@
 
 #### SETUP ####
 DOCKER_COMPOSE_FILE="${1:-docker-compose.yml}"
-
 DEFAULT_NODE_ENV=${NODE_ENV:-""}
-
 export NODE_ENV=test
 
+docker-compose -f ${DOCKER_COMPOSE_FILE} stop
 docker-compose -f ${DOCKER_COMPOSE_FILE} pull db
-
 echo "Creating Docker containers..."
 docker-compose -f ${DOCKER_COMPOSE_FILE} up --no-start
 echo "Starting DB..."
@@ -19,8 +17,6 @@ sleep 4
 # Need to create test DB separately because TypeORM won't do it for us
 docker exec -t -u postgres productcatalog_db_1 \
   psql --command "CREATE DATABASE test_${DB_NAME};"
-
-sleep 1
 
 docker-compose -f ${DOCKER_COMPOSE_FILE} run --rm server \
   yarn run migration:run -c test
@@ -46,9 +42,7 @@ docker exec -t -u postgres productcatalog_db_1 \
   psql --command "DROP DATABASE IF EXISTS test_${DB_NAME};"
 
 docker-compose -f ${DOCKER_COMPOSE_FILE} stop
-
 export NODE_ENV=${DEFAULT_NODE_ENV}
-
 docker-compose -f ${DOCKER_COMPOSE_FILE} up -d
 
 exit ${EXIT_CODE}
--- scripts/integration_tests.sh
+++ scripts/integration_tests.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
 DOCKER_COMPOSE_FILE="${1:-docker-compose.yml}"
+DEFAULT_NODE_ENV=${NODE_ENV:-""}
+export NODE_ENV=test
 
-docker-compose -f ${DOCKER_COMPOSE_FILE} up -d
-
-./scripts/wait-for-it.sh localhost:3333 -- echo "Server ready"
+docker-compose -f ${DOCKER_COMPOSE_FILE} stop
+docker-compose -f ${DOCKER_COMPOSE_FILE} pull db
+echo "Creating Docker containers..."
+docker-compose -f ${DOCKER_COMPOSE_FILE} up --no-start
+echo "Starting DB..."
+docker-compose -f ${DOCKER_COMPOSE_FILE} start db
 
 # Tests have been flaky in CI, probably due to the DB not being ready
 # even if the server is running. In other projects, a 4-second sleep
@@ -14,8 +19,6 @@ sleep 4
 # Need to create test DB separately because TypeORM won't do it for us
 docker exec -t -u postgres productcatalog_db_1 \
   psql --command "CREATE DATABASE test_${DB_NAME};"
-
-sleep 1
 
 docker-compose -f ${DOCKER_COMPOSE_FILE} run --rm server \
   yarn run migration:run -c test
@@ -32,5 +35,9 @@ fi
 # TEST CLEANUP
 docker exec -t -u postgres productcatalog_db_1 \
   psql --command "DROP DATABASE IF EXISTS test_${DB_NAME};"
+
+docker-compose -f ${DOCKER_COMPOSE_FILE} stop
+export NODE_ENV=${DEFAULT_NODE_ENV}
+docker-compose -f ${DOCKER_COMPOSE_FILE} up -d
 
 exit ${EXIT_CODE}
