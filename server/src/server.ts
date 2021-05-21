f97f5413a3408054b6efe297e2989dd4e80b7b00
melissa gattoni <meligatt@gmail.com>
2021-05-21 13:51:46 +1000

    move wildcard route definition to buildCustomAuthRouter function

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,8 @@
 import "reflect-metadata";
-import path from "path";
 import Koa from "koa";
 import { createConnection } from "typeorm";
 import { ApolloServer } from "apollo-server-koa";
 import helmet from "koa-helmet";
-import serveStatic from "koa-static";
-import send from "koa-send";
 import { Database, Resource } from "@admin-bro/typeorm";
 import AdminBro from "admin-bro";
 import { schema } from "./graphql/index";
@@ -40,29 +37,6 @@ createConnection(connectionName)
         entityManager: connection.manager,
       }),
     });
-
-    if (process.env.NODE_ENV === "production") {
-      // Something prepends the workingdirectory '/app' to the static files path,
-      // even if we use absolute paths for everything, but this works, so filo
-      const buildPath = path.join("dist", "build");
-
-      app.use(serveStatic(buildPath));
-
-      router.get("(.*)", async (ctx, next) => {
-        try {
-          await send(ctx, path.join(buildPath, "index.html"));
-        } catch (err) {
-          ctx.body =
-            "Ha ocurrido un error. Por favor, intente nuevamente. Comercial Gattoni.";
-          console.log(err);
-          return next();
-        }
-      });
-    } else {
-      router.get("/", async (ctx) => {
-        ctx.body = "Hello World!";
-      });
-    }
 
     app.use(helmet()).use(router.routes()).use(router.allowedMethods());
 
