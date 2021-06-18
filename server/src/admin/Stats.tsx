4578712b45af099c0aa9913878808eea286320b6
melissa gattoni <meligatt@gmail.com>
2021-06-18 10:27:33 +1000

    remove react video to check deployment issues

--- server/src/admin/Stats.tsx
+++ server/src/admin/Stats.tsx
@@ -1,11 +1,10 @@
-import { ApiClient } from "admin-bro";
 import { Box, Text , Section} from "@admin-bro/design-system";
 
 const Stats = () => {
   return (
     <Box variant="grey">
       <Section mb="xl">
-        <Text>Estadisticas Proximamente...</Text>
+        <Text>Seccion en construccion...</Text>
       </Section>
 
 
