991a3f5355966d627a62aeb4fcbc4740f80c0a23
Craig Franklin <craigjfranklin@gmail.com>
2022-10-16 16:22:11 +1100

    Update DB scripts to use CockroachDB

--- scripts/set_local_db_to_prod.sh
+++ scripts/set_local_db_to_prod.sh
@@ -7,14 +7,19 @@ FILE_ARG=${1:-""}
 
 if [ -z ${FILE_ARG} ]
 then
-  DB_FILE=prod_dump_`date +%Y-%m-%d"_"%H_%M_%S`.sql
-  DB_PATH=${PWD}/database/backups/${DB_FILE}
+  DB_FILE=prod_dump_`date +%Y-%m-%d"_"%H_%M_%S`
+  DB_PATH=database/backups/${DB_FILE}
 
   heroku pg:backups:capture --app=${HEROKU_APP}
-  heroku pg:backups:download --app=${HEROKU_APP} --output=${DB_PATH}
+  heroku pg:backups:download --app=${HEROKU_APP} --output=${DB_PATH}.dump
+  # Overwrite Heroku's custom format with plain-text SQL
+  pg_restore --no-owner -f ${DB_PATH}.sql ${DB_PATH}.dump
+  rm ${DB_PATH}.dump
+
+  DB_PATH=${DB_PATH}.sql
 else
   DB_FILE=${FILE_ARG}
-  DB_PATH=${PWD}/database/backups/${DB_FILE}
+  DB_PATH=database/backups/${DB_FILE}
 fi
 
 docker-compose rm -s -v db
@@ -25,6 +30,5 @@ docker-compose up -d db
 # so we're sleeping instead
 sleep 4
 
-cat ${DB_PATH} \
-  | docker exec -i productcatalog_db_1 pg_restore \
-    -U postgres -d ${DB_NAME} --verbose --no-owner
+docker exec -i productcatalog_db_1 \
+  cockroach sql --file ${DB_PATH} --insecure
