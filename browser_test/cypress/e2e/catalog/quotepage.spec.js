3536f90ce7b0fa2b83e5b211f460d8e07ed694f2
Craig Franklin <craigjfranklin@gmail.com>
2022-11-12 21:43:03 +1100

    consolidate Cypress tests and remove waits
    Just some tweaks to get the browser tests running slightly faster,
    since doing a full page load for each element check slows things
    down quite a bit.

--- browser_test/cypress/e2e/catalog/quotepage.spec.js
+++ browser_test/cypress/e2e/catalog/quotepage.spec.js
@@ -4,18 +4,15 @@ describe("Quote page", function(){
   beforeEach(() => {
     cy.visit("/cotizacion", { headers: { Connection: "Keep-Alive" }, responseTimeout: 31000 });
     cy.injectAxe();
-    cy.wait(1000);
   });
 
   describe("When page loads", () => {
     it("loads empty quote page", () => {
+      cy.checkA11y(null, null, terminalLog)
+
       cy.findByRole("heading",{ name: "Mi Cotización:" });
       cy.findByText("No hay Productos en la Cotización.");
       cy.findByRole("link",{ name: "Click aqui para empezar a agregar productos a su cotización." });
-    });
-
-    it('Has no detectable a11y violations on load (custom configuration)', () => {
-      cy.checkA11y(null, null, terminalLog)
     });
   });
 });
