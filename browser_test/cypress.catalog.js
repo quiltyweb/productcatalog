c6c5d4b2a9a1a01ad58038ae802c8057cff52b98
Craig Franklin <craigjfranklin@gmail.com>
2022-11-30 20:37:54 +1100

    improve Cypress config
    The retries made the tests too slow when there was a problem, and
    the large viewport made it impossible to see any detail in the UI.

--- browser_test/cypress.catalog.js
+++ browser_test/cypress.catalog.js
@@ -2,10 +2,6 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   video: false,
-  viewportHeight: 2000,
-  retries: {
-    runMode: 2
-  },
   e2e: {
     baseUrl: "http://localhost:3000",
     specPattern: "cypress/e2e/catalog/**/*.spec.{js,ts}",
