6c5466fabafda17eaf2752c8728a62b4d193078c
Craig Franklin <craigjfranklin@gmail.com>
2024-07-16 14:34:10 +1000

    Fix up CI for existing app

--- scripts/browser_tests.sh
+++ scripts/browser_tests.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-set -euo pipefail
+# set -euo pipefail
 
 #### SETUP ####
-DOCKER_COMPOSE_FILE="${1:-docker-compose.yml}"
-DEFAULT_NODE_ENV=${NODE_ENV:-""}
-export NODE_ENV=test
-DEFAULT_DB_NAME=${DB_NAME:-""}
-export DB_NAME=test_${DB_NAME}
+DOCKER_COMPOSE_FILE="${1:-docker-compose.ci.yml}"
 EXIT_CODE=0
 
 trap log_errors err
@@ -16,43 +12,28 @@ trap clean_up exit
 function log_errors() {
   docker-compose -f ${DOCKER_COMPOSE_FILE} ps
   docker-compose -f ${DOCKER_COMPOSE_FILE} logs
+  EXIT_CODE=1
 }
 
 function clean_up() {
-  docker-compose -f ${DOCKER_COMPOSE_FILE} exec -T db cockroach sql --execute "DROP DATABASE IF EXISTS ${DB_NAME};" --insecure
-
-  docker-compose -f ${DOCKER_COMPOSE_FILE} stop
-  export NODE_ENV=${DEFAULT_NODE_ENV}
-  export DB_NAME=${DEFAULT_DB_NAME}
-  docker-compose -f ${DOCKER_COMPOSE_FILE} up -d
+  docker-compose -f ${DOCKER_COMPOSE_FILE} exec -T db cockroach sql --execute "DROP DATABASE IF EXISTS test;" --insecure
 
   exit ${EXIT_CODE}
 }
 
-docker-compose -f ${DOCKER_COMPOSE_FILE} stop
-docker-compose -f ${DOCKER_COMPOSE_FILE} pull db
-docker-compose -f ${DOCKER_COMPOSE_FILE} up --no-start
-docker-compose -f ${DOCKER_COMPOSE_FILE} start db
-
-# --retry-all-errors is available in a version of curl that isn't available
-# in the latest version of Ubuntu yet, so we sleep till we can use it in CI.
-sleep 10
-
 # Need to create test DB separately because TypeORM won't do it for us
-docker-compose -f ${DOCKER_COMPOSE_FILE} exec -T db cockroach sql --execute "CREATE DATABASE IF NOT EXISTS ${DB_NAME};" --insecure
+docker-compose -f ${DOCKER_COMPOSE_FILE} exec -T db cockroach sql --execute "CREATE DATABASE IF NOT EXISTS test;" --insecure
 
-docker-compose -f ${DOCKER_COMPOSE_FILE} up -d
-
-docker-compose -f ${DOCKER_COMPOSE_FILE} run --rm server \
+docker-compose -f ${DOCKER_COMPOSE_FILE} exec -T server \
   yarn run migration:run -d src/dataSource.ts
 
 #### SEED TEST DB ####
 echo "Seeding database..."
-docker-compose -f ${DOCKER_COMPOSE_FILE} run --rm \
+docker-compose -f ${DOCKER_COMPOSE_FILE} exec -T \
   server yarn run ts-node test/fixtures/seed_db.ts
 
 #### RUN TESTS ####
 docker-compose -f ${DOCKER_COMPOSE_FILE} run --rm \
-  browser_test npm run run:catalog || EXIT_CODE=1
+  browser_test npm run run:catalog
 docker-compose -f ${DOCKER_COMPOSE_FILE} run --rm \
-  browser_test npm run run:admin || EXIT_CODE=1
+  browser_test npm run run:admin
--- scripts/integration_tests.sh
+++ scripts/integration_tests.sh
@@ -1,46 +1,25 @@
 #!/bin/bash
 
-DOCKER_COMPOSE_FILE="${1:-docker-compose.yml}"
-DEFAULT_NODE_ENV=${NODE_ENV:-""}
-export NODE_ENV=test
-DEFAULT_DB_NAME=${DB_NAME:-""}
-export DB_NAME=test_${DB_NAME}
-
-docker-compose -f ${DOCKER_COMPOSE_FILE} stop
-docker-compose -f ${DOCKER_COMPOSE_FILE} pull db
-echo "Creating Docker containers..."
-docker-compose -f ${DOCKER_COMPOSE_FILE} up --no-start
-echo "Starting DB..."
-docker-compose -f ${DOCKER_COMPOSE_FILE} start db
-
-# Tests have been flaky in CI, probably due to the DB not being ready
-# even if the server is running. In other projects, a 4-second sleep
-# safely give the DB time to get ready for input.
-sleep 4
+DOCKER_COMPOSE_FILE="${1:-docker-compose.ci.yml}"
 
 # Need to create test DB separately because TypeORM won't do it for us
-docker exec -t productcatalog_db_1 \
-  cockroach sql --execute "CREATE DATABASE ${DB_NAME};" --insecure
+docker-compose -f ${DOCKER_COMPOSE_FILE} exec -T db \
+  cockroach sql --execute "CREATE DATABASE IF NOT EXISTS test;" --insecure
 
-docker-compose -f ${DOCKER_COMPOSE_FILE} run --rm server \
+docker-compose -f ${DOCKER_COMPOSE_FILE} exec -T server \
   yarn run migration:run -d src/dataSource.ts
 
 EXIT_CODE=$?
 
 if [ ${EXIT_CODE} == 0 ]
 then
-  docker-compose -f ${DOCKER_COMPOSE_FILE} run --rm server yarn test:integration
+  docker-compose -f ${DOCKER_COMPOSE_FILE} exec -T server yarn test:integration
 
   EXIT_CODE=$?
 fi
 
 # TEST CLEANUP
-docker exec -t productcatalog_db_1 \
-  cockroach sql --execute "DROP DATABASE IF EXISTS ${DB_NAME};" --insecure
-
-docker-compose -f ${DOCKER_COMPOSE_FILE} stop
-export NODE_ENV=${DEFAULT_NODE_ENV}
-export DB_NAME=${DEFAULT_DB_NAME}
-docker-compose -f ${DOCKER_COMPOSE_FILE} up -d
+docker-compose -f ${DOCKER_COMPOSE_FILE} exec -T db \
+  cockroach sql --execute "DROP DATABASE IF EXISTS test;" --insecure
 
 exit ${EXIT_CODE}
